f51141935533d8bfdf8b336820e63564f2e7119b
Ittani maemo <42994867+ittani@users.noreply.github.com>
2023-05-05 00:29:21 +0200

    Add files via upload

---
+++ register.js
@@ -0,0 +1,29 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+  const email = document.querySelector('#email');
+
+form.addEventListener('submit', function(event) {
+  if (!email.checkValidity()) {
+    event.preventDefault();
+    alert('Please enter a valid email address.');
+  }
+});
